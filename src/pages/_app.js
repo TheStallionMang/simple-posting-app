1b663f83b106b2a471753d4bacc0d2469ff03538
Christian Barroso <barrosochristian3@gmail.com>
2023-10-11 11:01:16 -0700

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Navbar from "../../components/Navbar";
+import { Toaster } from "react-hot-toast";
+import { UserContext } from "../../lib/context";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { firestore } from "../../lib/firebase";
+import { useUserData } from "../../lib/hooks";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const userData = useUserData();
+
+  return (
+    <UserContext.Provider value={userData}>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </UserContext.Provider>
+  );
 }
